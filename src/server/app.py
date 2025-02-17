a91ba62ee604525d9521330c702204be56191f72
Mr-DooSun <mrdoosun@gmail.com>
2025-02-17 19:08:21 +0900

    add : health check

--- src/server/app.py
+++ src/server/app.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 
 from src.core.middleware.exception_handler import ExceptionMiddleware
+from src.server.application.controllers import health_check_controller
 from src.server.infrastructure.di.container import ServerContainer
 
 container = None
@@ -22,6 +23,8 @@ def create_app():
 
     app = FastAPI(docs_url="/docs")
     app.add_middleware(ExceptionMiddleware)
+
+    app.include_router(router=health_check_controller.router)
 
     return app
 
