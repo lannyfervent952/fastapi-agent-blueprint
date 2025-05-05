fdf6e48f32306576c05fcd8e7543a0589dba70c2
Mr-DooSun <mrdoosun@gmail.com>
2025-05-06 03:03:14 +0900

    sytle : response_model 추가

--- src/server/application/controllers/user_controller.py
+++ src/server/application/controllers/user_controller.py
@@ -6,39 +6,57 @@ from typing import List
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
-from src.core.applications.dtos.user_dto import CreateUserDto, UpdateUserDto
-from src.core.applications.responses.user_response import UserResponse
+from src.core.application.dtos.user_dto import CreateUserDto, UpdateUserDto
+from src.core.application.responses.base_response import BaseResponse
 from src.server.application.use_cases.user_use_case import UserUseCase
 from src.server.infrastructure.di.server_container import ServerContainer
 
 router = APIRouter()
 
 
-@router.post("/user", summary="유저 생성", tags=["유저"])
+@router.post(
+    "/user",
+    summary="유저 생성",
+    tags=["유저"],
+    response_model=BaseResponse,
+    response_model_exclude_none=True,
+)
 @inject
 async def create_user(
     create_data: CreateUserDto,
     user_use_case: UserUseCase = Depends(
         Provide[ServerContainer.user_container.user_use_case]
     ),
-) -> UserResponse:
+) -> BaseResponse:
     data = await user_use_case.create_data(create_data=create_data)
-    return UserResponse(data=data)
+    return BaseResponse(data=data)
 
 
-@router.post("/users", summary="유저 생성 (복수)", tags=["유저"])
+@router.post(
+    "/users",
+    summary="유저 생성 (복수)",
+    tags=["유저"],
+    response_model=BaseResponse,
+    response_model_exclude_none=True,
+)
 @inject
 async def create_users(
     create_datas: List[CreateUserDto],
     user_use_case: UserUseCase = Depends(
         Provide[ServerContainer.user_container.user_use_case]
     ),
-) -> UserResponse:
+) -> BaseResponse:
     data = await user_use_case.create_datas(create_datas=create_datas)
-    return UserResponse(data=data)
+    return BaseResponse(data=data)
 
 
-@router.get("/users", summary="유저 정보 모두 조회", tags=["유저"])
+@router.get(
+    "/users",
+    summary="유저 정보 모두 조회",
+    tags=["유저"],
+    response_model=BaseResponse,
+    response_model_exclude_none=True,
+)
 @inject
 async def get_users(
     page: int = 1,
@@ -46,24 +64,36 @@ async def get_users(
     user_use_case: UserUseCase = Depends(
         Provide[ServerContainer.user_container.user_use_case]
     ),
-) -> UserResponse:
+) -> BaseResponse:
     data, pagination = await user_use_case.get_datas(page=page, page_size=page_size)
-    return UserResponse(data=data, pagination=pagination)
+    return BaseResponse(data=data, pagination=pagination)
 
 
-@router.get("/user/{user_id}", summary="유저 정보 조회", tags=["유저"])
+@router.get(
+    "/user/{user_id}",
+    summary="유저 정보 조회",
+    tags=["유저"],
+    response_model=BaseResponse,
+    response_model_exclude_none=True,
+)
 @inject
 async def get_user_by_user_id(
     user_id: int,
     user_use_case: UserUseCase = Depends(
         Provide[ServerContainer.user_container.user_use_case]
     ),
-) -> UserResponse:
+) -> BaseResponse:
     data = await user_use_case.get_data_by_data_id(data_id=user_id)
-    return UserResponse(data=data)
+    return BaseResponse(data=data)
 
 
-@router.put("/user/{user_id}", summary="유저 수정", tags=["유저"])
+@router.put(
+    "/user/{user_id}",
+    summary="유저 수정",
+    tags=["유저"],
+    response_model=BaseResponse,
+    response_model_exclude_none=True,
+)
 @inject
 async def update_user_by_user_id(
     user_id: int,
@@ -71,20 +101,26 @@ async def update_user_by_user_id(
     user_use_case: UserUseCase = Depends(
         Provide[ServerContainer.user_container.user_use_case]
     ),
-) -> UserResponse:
+) -> BaseResponse:
     data = await user_use_case.update_data_by_data_id(
         data_id=user_id, update_data=update_data
     )
-    return UserResponse(data=data)
+    return BaseResponse(data=data)
 
 
-@router.delete("/user/{user_id}", summary="유저 삭제", tags=["유저"])
+@router.delete(
+    "/user/{user_id}",
+    summary="유저 삭제",
+    tags=["유저"],
+    response_model=BaseResponse,
+    response_model_exclude_none=True,
+)
 @inject
 async def delete_user_by_user_id(
     user_id: int,
     user_use_case: UserUseCase = Depends(
         Provide[ServerContainer.user_container.user_use_case]
     ),
-) -> UserResponse:
+) -> BaseResponse:
     await user_use_case.delete_data_by_data_id(data_id=user_id)
-    return UserResponse()
+    return BaseResponse()
