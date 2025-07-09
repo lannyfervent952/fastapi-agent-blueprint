f0793b677bf7b55e8936252b1780e144aa5f4e50
Mr-DooSun <mrdoosun@gmail.com>
2025-07-09 20:40:55 +0900

    feat : 컬럼  라벨링, 필터, 검색 기능, 정령 추가

--- src/server/admin/views/user_view.py
+++ src/server/admin/views/user_view.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 from sqladmin import ModelView
-from sqlalchemy import class_mapper
+from sqlalchemy.orm import class_mapper
 
 from src.core.infrastructure.database.models.user_model import UserModel
 
 
 class UserView(ModelView, model=UserModel):
+    category = "유저"
+    name = "유저 목록"
     column_list = [attr.key for attr in class_mapper(UserModel).column_attrs]
+
+    column_labels = {
+        "id": "ID",
+        "username": "이름",
+        "email": "이메일",
+        "created_at": "가입일",
+        "updated_at": "수정일",
+    }
+
+    column_searchable_list = ["username", "email"]
+    column_filters = ["created_at"]
+    column_sortable_list = ["id", "created_at"]
