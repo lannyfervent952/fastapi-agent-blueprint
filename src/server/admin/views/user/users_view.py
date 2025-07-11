2b42725cbaa6ca5adf5edca8a087b35f74a7ced6
Mr-DooSun <mrdoosun@gmail.com>
2025-07-11 22:19:58 +0900

    refac : remove literal

--- src/server/admin/views/user/users_view.py
+++ src/server/admin/views/user/users_view.py
@@ -5,18 +5,19 @@ from sqlalchemy.orm import class_mapper
 from src.core.infrastructure.database.models.user.users_model import UsersModel
 
 
-class UsersView(ModelView, model=UsersModel):
+class UserView(ModelView, model=UsersModel):
     category = "유저"
     name = "유저 목록"
     column_list = [attr.key for attr in class_mapper(UsersModel).column_attrs]
 
     column_labels = {
-        "id": "ID",
-        "username": "이름",
-        "email": "이메일",
-        "created_at": "가입일",
-        "updated_at": "수정일",
+        UsersModel.id: "ID",
+        UsersModel.username: "이름",
+        UsersModel.email: "이메일",
+        UsersModel.password: "비밀번호",
+        UsersModel.created_at: "가입일",
+        UsersModel.updated_at: "수정일",
     }
 
-    column_searchable_list = ["username", "email"]
-    column_sortable_list = ["id", "created_at"]
+    column_searchable_list = [UsersModel.id, UsersModel.username, UsersModel.email]
+    column_sortable_list = [UsersModel.id, UsersModel.username, UsersModel.created_at]
