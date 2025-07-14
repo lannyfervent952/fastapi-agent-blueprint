ec77f67cf7b4beb86d4a5525a47f6656e1db01f6
Mr-DooSun <mrdoosun@gmail.com>
2025-07-15 02:26:51 +0900

    style : responseDTO  bound 변경

--- src/core/application/use_cases/base_use_case.py
+++ src/core/application/use_cases/base_use_case.py
@@ -5,16 +5,13 @@ from typing import List, Tuple, Type, TypeVar
 from pydantic import BaseModel
 
 from src.core.application.dtos.common.base_request import IdListDto
-from src.core.application.dtos.common.base_response import (
-    PaginationInfo,
-    SuccessResponse,
-)
+from src.core.application.dtos.common.base_response import PaginationInfo
 from src.core.common.pagination import make_pagination
 from src.core.domain.services.base_service import BaseService
 
 CreateDTO = TypeVar("CreateDTO", bound=BaseModel)
 UpdateDTO = TypeVar("UpdateDTO", bound=BaseModel)
-SuccessResponse = TypeVar("SuccessResponse", bound=SuccessResponse)
+ResponseDTO = TypeVar("ResponseDTO", bound=BaseModel)
 
 
 class BaseUseCase(ABC):
@@ -31,7 +28,7 @@ class BaseUseCase(ABC):
 
     @property
     @abstractmethod
-    def response_dto(self) -> Type[SuccessResponse]:
+    def response_dto(self) -> Type[ResponseDTO]:
         pass
 
     @property
@@ -39,17 +36,15 @@ class BaseUseCase(ABC):
     def update_dto(self) -> Type[UpdateDTO]:
         pass
 
-    async def create_data(self, create_data: CreateDTO) -> SuccessResponse:
+    async def create_data(self, create_data: CreateDTO) -> ResponseDTO:
         return await self.base_service.create_data(create_data=create_data)
 
-    async def create_datas(
-        self, create_datas: List[CreateDTO]
-    ) -> List[SuccessResponse]:
+    async def create_datas(self, create_datas: List[CreateDTO]) -> List[ResponseDTO]:
         return await self.base_service.create_datas(create_datas=create_datas)
 
     async def get_datas(
         self, page: int, page_size: int
-    ) -> Tuple[List[SuccessResponse], PaginationInfo]:
+    ) -> Tuple[List[ResponseDTO], PaginationInfo]:
         datas = await self.base_service.get_datas(page=page, page_size=page_size)
 
         total_items = await self.base_service.count_datas()
@@ -59,15 +54,15 @@ class BaseUseCase(ABC):
 
         return datas, pagination
 
-    async def get_data_by_data_id(self, data_id: int) -> SuccessResponse:
+    async def get_data_by_data_id(self, data_id: int) -> ResponseDTO:
         return await self.base_service.get_data_by_data_id(data_id=data_id)
 
-    async def get_datas_by_data_ids(self, payload: IdListDto) -> List[SuccessResponse]:
+    async def get_datas_by_data_ids(self, payload: IdListDto) -> List[ResponseDTO]:
         return await self.base_service.get_datas_by_data_ids(data_ids=payload.ids)
 
     async def update_data_by_data_id(
         self, data_id: int, update_data: UpdateDTO
-    ) -> SuccessResponse:
+    ) -> ResponseDTO:
         return await self.base_service.update_data_by_data_id(
             data_id=data_id, update_data=update_data
         )
