6518b50e57ee84ad47aea834df415bd80e5d698c
Mr-DooSun <mrdoosun@gmail.com>
2025-07-16 20:05:02 +0900

    style : rename controller -> router

--- src/server/infrastructure/bootstrap/route_registry.py
+++ src/server/infrastructure/bootstrap/route_registry.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from fastapi import FastAPI
 
-from src.server.application.controllers import health_check_controller
-from src.server.application.controllers.user import users_controller
+from src.server.application.routers import health_check_router
+from src.server.application.routers.user import users_router
 
 
 def register_routes(app: FastAPI):
-    app.include_router(
-        router=health_check_controller.router, prefix="/v1", tags=["status"]
-    )
-    app.include_router(router=users_controller.router, prefix="/v1", tags=["유저"])
+    app.include_router(router=health_check_router.router, prefix="/v1", tags=["status"])
+    app.include_router(router=users_router.router, prefix="/v1", tags=["유저"])
