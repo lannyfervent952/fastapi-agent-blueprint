5512243c5460993160ccc324148cb68e5c3431d4
Mr-DooSun <mrdoosun@gmail.com>
2025-07-16 23:52:34 +0900

    feat : websocket 추가 하기

--- src/server/app.py
+++ src/server/app.py
@@ -4,7 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.core.middleware.exception_middleware import ExceptionMiddleware
 from src.server.infrastructure.bootstrap.admin_registry import setup_admin_views
-from src.server.infrastructure.bootstrap.route_registry import register_routes
+from src.server.infrastructure.bootstrap.api_route_registry import register_api_routes
+from src.server.infrastructure.bootstrap.websocket_route_registry import (
+    register_websocket_routes,
+)
 from src.server.infrastructure.di.server_container import ServerContainer
 
 container = None
@@ -30,7 +33,8 @@ def create_app():
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    register_routes(app=app)
+    register_api_routes(app=app)
+    register_websocket_routes(app=app)
     setup_admin_views(app=app, database=container.core_container.database())
 
     return app
--- src/server/infrastructure/bootstrap/api_route_registry.py
+++ src/server/infrastructure/bootstrap/api_route_registry.py
@@ -5,6 +5,6 @@ from src.server.application.routers.api import health_check_router
 from src.server.application.routers.api.user import users_router
 
 
-def register_routes(app: FastAPI):
+def register_api_routes(app: FastAPI):
     app.include_router(router=health_check_router.router, prefix="/v1", tags=["status"])
     app.include_router(router=users_router.router, prefix="/v1", tags=["유저"])
