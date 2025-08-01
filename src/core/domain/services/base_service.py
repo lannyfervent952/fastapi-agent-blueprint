11712762bab32c780361e36fa14fa6780432b426
Mr-DooSun <mrdoosun@gmail.com>
2025-08-02 01:54:19 +0900

    refac : 중복 model_validate 제거

--- src/core/domain/services/base_service.py
+++ src/core/domain/services/base_service.py
@@ -25,35 +25,21 @@ class BaseService(Generic[CreateEntity, ReturnEntity, UpdateEntity], ABC):
         self.update_entity = update_entity
 
     async def create_data(self, create_data: CreateEntity) -> ReturnEntity:
-        data = await self.base_repository.create_data(create_data=create_data)
-        return self.return_entity.model_validate(data, from_attributes=True)
+        return await self.base_repository.create_data(create_data=create_data)
 
     async def create_datas(
         self, create_datas: List[CreateEntity]
     ) -> List[ReturnEntity]:
-        datas = await self.base_repository.create_datas(create_datas=create_datas)
-        return [
-            self.return_entity.model_validate(data, from_attributes=True)
-            for data in datas
-        ]
+        return await self.base_repository.create_datas(create_datas=create_datas)
 
     async def get_datas(self, page: int, page_size: int) -> List[ReturnEntity]:
-        datas = await self.base_repository.get_datas(page=page, page_size=page_size)
-        return [
-            self.return_entity.model_validate(data, from_attributes=True)
-            for data in datas
-        ]
+        return await self.base_repository.get_datas(page=page, page_size=page_size)
 
     async def get_data_by_data_id(self, data_id: int) -> ReturnEntity:
-        data = await self.base_repository.get_data_by_data_id(data_id=data_id)
-        return self.return_entity.model_validate(data, from_attributes=True)
+        return await self.base_repository.get_data_by_data_id(data_id=data_id)
 
     async def get_datas_by_data_ids(self, data_ids: List[int]) -> List[ReturnEntity]:
-        datas = await self.base_repository.get_datas_by_data_ids(data_ids=data_ids)
-        return [
-            self.return_entity.model_validate(data, from_attributes=True)
-            for data in datas
-        ]
+        return await self.base_repository.get_datas_by_data_ids(data_ids=data_ids)
 
     async def count_datas(self) -> int:
         return await self.base_repository.count_datas()
@@ -61,10 +47,9 @@ class BaseService(Generic[CreateEntity, ReturnEntity, UpdateEntity], ABC):
     async def update_data_by_data_id(
         self, data_id: int, update_data: UpdateEntity
     ) -> ReturnEntity:
-        data = await self.base_repository.update_data_by_data_id(
+        return await self.base_repository.update_data_by_data_id(
             data_id=data_id, update_data=update_data
         )
-        return self.return_entity.model_validate(data, from_attributes=True)
 
     async def delete_data_by_data_id(self, data_id: int) -> bool:
         return await self.base_repository.delete_data_by_data_id(data_id=data_id)
