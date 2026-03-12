02aa91529efaea6330f9b1a480a7c6e7fcdab986
Mr-DooSun <mrdoosun@gmail.com>
2026-03-12 22:54:28 +0900

    fix: remove remaining Entity traces after DTO migration
    - Replace UserEntity with UserDTO in user_test_task.py
    - Fix process_user kwarg: entity= → dto=
    - Delete empty src/_core/domain/entities/ directory

--- src/user/interface/worker/tasks/user_test_task.py
+++ src/user/interface/worker/tasks/user_test_task.py
@@ -2,7 +2,7 @@ from dependency_injector.wiring import Provide, inject
 
 from src._apps.worker.broker import broker
 from src.user.application.use_cases.user_use_case import UserUseCase
-from src.user.domain.entities.user_entity import UserEntity
+from src.user.domain.dtos.user_dto import UserDTO
 from src.user.infrastructure.di.user_container import UserContainer
 
 
@@ -12,6 +12,6 @@ async def consume_task(
     user_use_case: UserUseCase = Provide[UserContainer.user_use_case],
     **kwargs,
 ) -> None:
-    entity = UserEntity.model_validate(kwargs)
+    dto = UserDTO.model_validate(kwargs)
 
-    await user_use_case.process_user(entity=entity)
+    await user_use_case.process_user(dto=dto)
